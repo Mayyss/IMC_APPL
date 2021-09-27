b7cb29f113590281bf4357b22581982b4a4e68ac
MAYSSA MARTHA FRANCISCO DA SILVA <21193395@CFP127ALUNO.LOCAL>
2021-09-27 16:01:18 -0300

    Adicionado os button e uma textView sobre a senha e conta

--- app/src/main/java/com/example/imcappli/MainActivity.kt
+++ app/src/main/java/com/example/imcappli/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Pra esconder o toolbar/actionBar
+        // escoder - hide
+        //double bang => !!
+        //Se tiver toolbar esconda, se não tiver faça nada
+        supportActionBar!!.hide()
     }
 }
